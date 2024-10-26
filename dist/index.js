61dd205eb0f694cfc310f4bfaffdd7e6d754af5a
TableStar <afraroad@gmail.com>
2024-10-27 01:24:21 +0700

    feat: add books display

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,45 @@
 "use strict";
+const myLibrary = [
+    {
+        title: "The Twin Towers",
+        author: "J.R.R Tolkien",
+        pages: 500,
+        isRead: false,
+    },
+];
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
+}
+Book.prototype.toggleRead = function () {
+    this.isRead = !this.isRead;
+};
+const renderBook = () => {
+    const bookContainer = document.querySelector(".book-container");
+    if (myLibrary) {
+        const newBooks = myLibrary.map((book) => {
+            const div = document.createElement("div");
+            div.className = "books";
+            div.innerHTML = `<div class="book-stats">
+                <h2>${book.title}</h2>
+                <p>Author: ${book.author}</p>
+                <p>Deskripsi</p>
+                <p>Total Pages: ${book.pages}</p>
+                <p>${book.isRead
+                ? "You have read this book"
+                : "You haven't read this book"}</p>
+        </div>
+                <div class="book-buttons">
+                    <button class="remove-button">Remove</button>
+                    <button class="read-button">Read</button>
+                </div>`;
+            return div;
+        });
+        bookContainer.append(...newBooks);
+    }
+};
+renderBook();
---
+++ src/index.ts
@@ -0,0 +1,67 @@
+interface BookInterface {
+  title: string;
+  author: string;
+  pages: number;
+  isRead: boolean;
+}
+
+const myLibrary: BookInterface[] = [
+  {
+    title: "The Twin Towers",
+    author: "J.R.R Tolkien",
+    pages: 500,
+    isRead: false,
+  },
+];
+
+class Book implements BookInterface {
+  constructor(
+    public title: string,
+    public author: string,
+    public pages: number,
+    public isRead: boolean
+  ) {}
+}
+
+interface Book {
+  toggleRead: () => void;
+}
+
+Book.prototype.toggleRead = function () {
+  this.isRead = !this.isRead;
+};
+
+const addBook = () => {
+  
+}
+
+const renderBook = () => {
+  const bookContainer = document.querySelector(".book-container");
+  if (myLibrary) {
+    const newBooks = myLibrary.map((book) => {
+      const div = document.createElement("div");
+      div.className = "books";
+
+      div.innerHTML = `<div class="book-stats">
+                <h2>${book.title}</h2>
+                <p>Author: ${book.author}</p>
+                <p>Deskripsi</p>
+                <p>Total Pages: ${book.pages}</p>
+                <p>${
+                  book.isRead
+                    ? "You have read this book"
+                    : "You haven't read this book"
+                }</p>
+        </div>
+                <div class="book-buttons">
+                    <button class="remove-button">Remove</button>
+                    <button class="read-button">Read</button>
+                </div>`;
+      return div;
+    });
+
+    bookContainer!.append(...newBooks);
+  }
+};
+
+renderBook();
